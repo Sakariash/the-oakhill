416e8d534237393b22492c99db29d3d8ee2c1059
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-05-28 19:50:04 +0200

    Update Storyblok API key configuration

--- libs/storyblok.js
+++ libs/storyblok.js
@@ -1,7 +1,7 @@
 import StoryblokClient from 'storyblok-js-client';
 
 const Storyblok = new StoryblokClient({
-  accessToken: 'your-storyblok-access-token'
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN
 });
 
 export default Storyblok;
