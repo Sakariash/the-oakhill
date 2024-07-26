f6d899f049bee7949c28f08a6e09a9cc41357d04
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-07-26 18:49:45 +0200

    feat: Fixed layout duplications

--- porfolio-demo/pages/[slug].js
+++ porfolio-demo/pages/[slug].js
@@ -6,8 +6,8 @@ import {
   getStoryblokApi,
   StoryblokComponent,
 } from "@storyblok/react";
- 
-export default function Page({ story }) {
+
+export default function Page({ story, config }) {
   story = useStoryblokState(story);
  
   return (
@@ -16,9 +16,7 @@ export default function Page({ story }) {
         <title>{story ? story.name : "My Site"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
         <StoryblokComponent blok={story.content} />
-      </Layout>
     </div>
   );
 }
@@ -32,11 +30,13 @@ export async function getStaticProps({ params }) {
   
     const storyblokApi = getStoryblokApi();
     let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+    let { data: config } = await storyblokApi.get('cdn/stories/config', sbParams);
   
     return {
       props: {
         story: data ? data.story : false,
         key: data ? data.story.id : false,
+        config: config ? config.story : false,
       },
       revalidate: 3600,
     };
@@ -58,60 +58,3 @@ export async function getStaticPaths() {
       fallback: false,
     };
   }
-// import Head from "next/head";
-// import {
-//     useStoryblokState,
-//     getStoryblokApi,
-//     StoryblokComponent,
-//   } from "@storyblok/react";
-//   import Layout from "../components/Layout";
-  
-//   export default function DynamicPage({ story }) {
-//     story = useStoryblokState(story);
-  
-//     return (
-//       <div>
-//         <Head>
-//           <title>Create Next App</title>
-//           <link rel="icon" href="/favicon.ico" />
-//         </Head>
-//         <Layout>
-//           <StoryblokComponent blok={story.content} />
-//         </Layout>
-//       </div>
-//     );
-//   }
-  
-//   export async function getStaticPaths() {
-//     const storyblokApi = getStoryblokApi();
-//     const { data } = await storyblokApi.get("cdn/stories");
-//     const paths = data.stories.map((story) => ({
-//       params: { slug: story.slug },
-//     }));
-  
-//     return {
-//       paths,
-//       fallback: false, // Set fallback to false if all possible slugs are known
-//     };
-//   }
-  
-//   export async function getStaticProps({ params }) {
-//     const slug = params.slug || "home";
-//     const sbParams = {
-//       version: "draft", // or 'published'
-//       resolve_links: "url",
-//     };
-  
-//     const storyblokApi = getStoryblokApi();
-//     const { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
-//     const { data: config } = await storyblokApi.get("cdn/stories/config");
-  
-//     return {
-//       props: {
-//         story: data ? data.story : false,
-//         key: data ? data.story.id : false,
-//         config: config ? config.story : false,
-//       },
-//       revalidate: 3600,
-//     };
-//   }
