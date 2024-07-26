7a2b36248fc16538fa5ac68917ca0df194877e46
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-07-26 18:52:50 +0200

    feat: Add image and text

--- porfolio-demo/components/Teaser.js
+++ porfolio-demo/components/Teaser.js
@@ -1,9 +1,25 @@
-import { storyblokEditable } from "@storyblok/react";
- 
+import Image from 'next/image';
+import { storyblokEditable } from '@storyblok/react';
+
 const Teaser = ({ blok }) => {
-  return <h2 className="text-2xl mb-10" {...storyblokEditable(blok)}>
-    {blok.headline}
-  </h2>;
+  return (
+    <div {...storyblokEditable(blok)}>
+      <h2 className="text-2xl mb-10">{blok.headline}</h2>
+      {blok.image && blok.image.filename && (
+        // <div style={{ width: '300px', height: 'auto',  }}>
+        <Image
+           src={blok.image.filename}
+          //  layout="responsive"
+           width={500}
+           height={500}
+           objectFit="cover" 
+           alt={blok.image.alt || 'Image'}
+        />
+        // </div>
+      )}
+      <p>{blok.info}</p>
+    </div>
+  );
 };
- 
+
 export default Teaser;
