c5c531fac056d2feaebf7be32055822c149c6117
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-07-28 02:13:59 +0200

    feat: make card available for threecolumn

--- porfolio-demo/components/ThreeColumn.js
+++ porfolio-demo/components/ThreeColumn.js
@@ -1,5 +1,6 @@
-import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
-import Teaser from './Teaser'; // Import Teaser component
+import { storyblokEditable } from "@storyblok/react";
+import Teaser from './Teaser';
+import Card from "./Card";
 
 const ThreeColumn = ({ blok }) => {
   return (
@@ -7,8 +8,8 @@ const ThreeColumn = ({ blok }) => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {blok.columns.map((nestedBlok) => (
           <div key={nestedBlok._uid} className="w-full">
-            {/* Use Teaser component without additional props */}
-            <Teaser blok={nestedBlok} isThreeColumn />
+            {nestedBlok.component === 'teaser' && <Teaser blok={nestedBlok} isThreeColumn />}
+            {nestedBlok.component === 'card' && <Card blok={nestedBlok} isThreeColumn />}
           </div>
         ))}
       </div>
