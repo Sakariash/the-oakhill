8e4e53a2cc63b8c05d4716095dc475ff89eea846
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-08-03 13:20:35 +0200

    feat: small fixes

--- porfolio-demo/components/TwoImage.js
+++ porfolio-demo/components/TwoImage.js
@@ -3,9 +3,13 @@ import { storyblokEditable } from '@storyblok/react';
 
 const TwoImages = ({ blok }) => {
   return (
-    <div {...storyblokEditable(blok)} className="relative h-screen mx-4 py-4 flex bg-red-200 md: mb-6">
-      <div className="relative flex-1 h-full hidden md:flex ">
-        <div className="absolute top-0 left-0 w-[calc(50%+3%)] h-full" style={{ clipPath: 'polygon(0 0, 100% 0, 87% 100%, 0 100%)' }}>
+    <div {...storyblokEditable(blok)} className="relative h-screen mx-4 py-4 flex bg-red-200 mb-6">
+      {/* Desktop View */}
+      <div className="relative flex-1 h-full hidden md:flex">
+        <div
+          className="absolute top-0 left-0 w-[calc(50%+3%)] h-full"
+          style={{ clipPath: 'polygon(0 0, 100% 0, 87% 100%, 0 100%)' }}
+        >
           {blok.Images[0]?.image?.filename && (
             <div className="relative h-full w-full">
               <Image
@@ -17,7 +21,10 @@ const TwoImages = ({ blok }) => {
             </div>
           )}
         </div>
-        <div className="absolute top-0 right-0 w-[calc(50%+3%)] h-full" style={{ clipPath: 'polygon(13% 0, 100% 0%, 100% 100%, 0% 100%)' }}>
+        <div
+          className="absolute top-0 right-0 w-[calc(50%+3%)] h-full"
+          style={{ clipPath: 'polygon(13% 0, 100% 0, 100% 100%, 0 100%)' }}
+        >
           {blok.Images[1]?.image?.filename && (
             <div className="relative h-full w-full">
               <Image
@@ -33,26 +40,32 @@ const TwoImages = ({ blok }) => {
 
       {/* Mobile/Tablet View */}
       <div className="relative flex flex-col w-full min-h-full bg-blue-200 md:hidden">
-          <div className="absolute top-0 w-full h-[calc(50%+3%)]" style={{ clipPath: 'polygon(0 0, 100% 0, 100% 87%, 0 100%)' }}>
-            {blok.Images[0]?.image?.filename && (
-              <Image
-                src={blok.Images[0].image.filename}
-                alt={blok.Images[0].image.alt || 'First Image'}
-                layout="fill"
-                objectFit="cover"
-              />
-            )}
-          </div>
-          <div className="absolute bottom-0 w-full h-[calc(50%+3%)]" style={{ clipPath: 'polygon(0 13%, 100% 0, 100% 100%, 0 100%)' }}>
-            {blok.Images[1]?.image?.filename && (
-              <Image
-                 src={blok.Images[1].image.filename}
-                alt={blok.Images[1].image.alt || 'Second Image'}
-                layout="fill"
-                objectFit="cover"
-              />
-            )}
-          </div>
+        <div
+          className="absolute top-0 w-full h-[calc(50%+3%)]"
+          style={{ clipPath: 'polygon(0 0, 100% 0, 100% 87%, 0 100%)' }}
+        >
+          {blok.Images[0]?.image?.filename && (
+            <Image
+              src={blok.Images[0].image.filename}
+              alt={blok.Images[0].image.alt || 'First Image'}
+              layout="fill"
+              objectFit="cover"
+            />
+          )}
+        </div>
+        <div
+          className="absolute bottom-0 w-full h-[calc(50%+3%)]"
+          style={{ clipPath: 'polygon(0 13%, 100% 0, 100% 100%, 0 100%)' }}
+        >
+          {blok.Images[1]?.image?.filename && (
+            <Image
+              src={blok.Images[1].image.filename}
+              alt={blok.Images[1].image.alt || 'Second Image'}
+              layout="fill"
+              objectFit="cover"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
