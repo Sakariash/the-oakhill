84368cf47ea87348725d324f61dd139a57c70a15
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-08-15 23:15:27 +0200

    feat: fix faq design

--- porfolio-demo/components/FAQ.js
+++ porfolio-demo/components/FAQ.js
@@ -10,7 +10,7 @@ const FAQ = ({ blok }) => {
   };
 
   return (
-    <div {...storyblokEditable(blok)} className="w-auto py-14">
+    <div {...storyblokEditable(blok)} className="w-auto mt-14">
       <h2 className="text-7xl font-diatype mb-10 md:mx-[100px] text-left">FAQ</h2>
       {blok.questions.map((faq, index) => (
         <div 
@@ -23,14 +23,14 @@ const FAQ = ({ blok }) => {
             className={"flex items-center cursor-pointer py-6"}
             onClick={() => toggleAnswer(index)}
           >
-            <h3 className="text-xl font-diatype text-left flex-1 text-4xl">{faq.question}</h3>
+            <h3 className="font-diatype text-left flex-1 text-lg md:text-2xl lg:text-4xl">{faq.question}</h3>
             <span className={`text-xl transition-transform duration-300 ease-in-out`}>
               {activeIndex === index ? <FaChevronUp /> : <FaChevronDown />}
             </span>
           </div>
-          <div className={`overflow-hidden text-left transition-all duration-300 ease-in-out
-            ${activeIndex === index ? 'max-h-96 py-4' : 'max-h-0 py-0'}`}>
-            <p className="text-lg">{faq.answer}</p>
+          <div className={`overflow-hidden text-left transition-all duration-300 ease-in-out w-2/3
+            ${activeIndex === index ? 'max-h-full py-4' : 'max-h-0 py-0'}`}>
+            <p className="text-lg whitespace-pre-wrap">{faq.answer}</p>
           </div>
         </div>
       ))}
