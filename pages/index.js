426e45c0ad524aa36203edd1e9545573f2ae5efa
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-08-16 23:57:36 +0200

    fix: favicon

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ story, config }) {
     <div>
       <Head>
         <title>Oakhill Design&Solutions</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;700;900&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;700&family=Montserrat:ital,wght@0,100;0,200;0,400;0,700;1,100;1,200;1,400;1,700&display=swap" rel="stylesheet"/>
       </Head>
