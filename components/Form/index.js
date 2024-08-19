ac3b32fd149ca66d3d84052d167c844c8b1541f3
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-08-20 00:07:27 +0200

    feat: form design updated

--- components/Form/index.js
+++ components/Form/index.js
@@ -4,13 +4,15 @@ import { storyblokEditable } from '@storyblok/react';
 const GenericForm = ({ blok }) => {
   const [formData, setFormData] = useState({
     name: '',
-    lastname: '',
-    address: '',
-    description: '',
+    company: '',
+    email: '',
+    message: '',
+    phonenumber: '',
     checklist: [],
   });
   const [errors, setErrors] = useState({});
   const [notification, setNotification] = useState(null);
+  const [focusedField, setFocusedField] = useState(null);
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -28,8 +30,9 @@ const GenericForm = ({ blok }) => {
 
   const validateForm = () => {
     const newErrors = {};
-    if (!formData.name) newErrors.name = 'Vänligen fyll i ditt namn.';
-    if (!formData.description) newErrors.description = 'Vänligen lämna en beskrivning av jobbet ni vill få en offert på.';
+    if (!formData.name) newErrors.name = 'Vänligen fyll i ert namn.';
+    if (!formData.email) newErrors.email = 'Vänligen fyll i er email';
+    if (!formData.message) newErrors.message = 'Vänligen lämna en beskrivning av jobbet ni vill få en offert på.';
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -47,119 +50,157 @@ const GenericForm = ({ blok }) => {
         });
 
         if (response.ok) {
-          setNotification({ type: 'success', message: 'Tack, vi har tagot emot dit email!' });
+          setNotification({ type: 'success', message: 'Tack, vi har tagit emot ditt mail!' });
           setFormData({
             name: '',
-            lastname: '',
-            address: '',
-            description: '',
+            company: '',
+            email: '',
+            phonenumber: '',
+            message: '',
             checklist: [],
           });
           setErrors({});
         } else {
-          setNotification({ type: 'error', message: 'Ett fel uppstod, vänligen försök igen!.' });
+          setNotification({ type: 'error', message: 'Ett fel uppstod, vänligen försök igen!' });
         }
       } catch (error) {
         console.error('Error sending email:', error);
-        setNotification({ type: 'error', message: 'Ett fel uppstod, vänligen försök igen!.' });
+        setNotification({ type: 'error', message: 'Ett fel uppstod, vänligen försök igen!' });
       }
     }
   };
 
+  const handleFocus = (field) => {
+    setFocusedField(field);
+  };
+
+  const handleBlur = () => {
+    setFocusedField(null);
+  };
+
+  const getLabelClasses = (field) => {
+    const isFocused = focusedField === field;
+    const isFilled = formData[field] !== '';
+    return `absolute left-1 text-gray-700 font-medium transition-transform duration-500 ease-in-out ${
+      isFilled || isFocused
+        ? 'transform -translate-y-3 scale-75 top-0'
+        : 'top-4'
+    }`;
+  };
+
   return (
-    <div {...storyblokEditable(blok)} className="relative max-w-4xl mx-auto font-montserrat text-left p-6 shadow-md rounded-lg border border-oakhill-green">
+    <div {...storyblokEditable(blok)} className="relative max-w-4xl mx-3 md:mx-auto font-montserrat text-left p-6 shadow-md rounded-lg border border-oakhill-green">
       <div className="absolute top-0 left-0 w-full h-2 bg-oakhill-green rounded-t-lg"></div>
       <h2 className="font-montserrat mb-6 text-2xl text-left text-gray-800 border-l-4 border-oakhill-green pl-4">
-        {blok.title || 'Contact Form'}
+        {blok.title || 'Kontakta oss'}
       </h2>
       <form onSubmit={handleSubmit} className="space-y-6">
-        {blok.name && (
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 font-medium mb-1 px-2">
-              Namn
+        <div className="flex space-x-4">
+          {blok.name && (
+            <div className="relative w-1/2">
+              <label
+                htmlFor="name"
+                className={getLabelClasses('name')}
+              >
+                Namn
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                onFocus={() => handleFocus('name')}
+                onBlur={handleBlur}
+                className={`w-full px-2 py-2 border-b-2 ${errors.name ? 'border-red-500' : 'border-gray-300'} focus:outline-none`}
+              />
+              {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+            </div>
+          )}
+          {blok.company !== undefined && (
+            <div className="relative w-1/2">
+              <label
+                htmlFor="company"
+                className={getLabelClasses('company')}
+              >
+              Företag
+              </label>
+              <input
+                type="text"
+                id="company"
+                name="company"
+                value={formData.company}
+                onChange={handleChange}
+                onFocus={() => handleFocus('company')}
+                onBlur={handleBlur}
+                className={`w-full px-2 py-2 border-b-2 ${errors.company ? 'border-red-500' : 'border-gray-300'} focus:outline-none`}
+              />
+              {errors.company && <p className="text-red-500 text-sm mt-1">{errors.company}</p>}
+            </div>
+          )}
+        </div>
+        {blok.email !== undefined && (
+          <div className="relative mb-4">
+            <label
+              htmlFor="email"
+              className={getLabelClasses('email')}
+            >
+            Email
             </label>
             <input
               type="text"
-              id="name"
-              name="name"
-              value={formData.name}
+              id="email"
+              name="email"
+              value={formData.email}
               onChange={handleChange}
-              className={`w-full px-3 py-2 border rounded-lg ${errors.name ? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              onFocus={() => handleFocus('email')}
+              onBlur={handleBlur}
+              className={`w-full px-2 py-2 border-b-2 ${errors.email ? 'border-red-500' : 'border-gray-300'} focus:outline-none`}
             />
-            {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name}</p>}
+            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
           </div>
         )}
-        {blok.lastname !== undefined && (
-          <div className="mb-4">
-            <label htmlFor="lastname" className="block text-gray-700 font-medium mb-1 px-2">
-              Efternamn
+        {blok.phonenumber !== undefined && (
+          <div className="relative mb-4">
+            <label
+              htmlFor="phonenumber"
+              className={getLabelClasses('phonenumber')}
+            >
+              Telefon
             </label>
             <input
               type="text"
-              id="lastname"
-              name="lastname"
-              value={formData.lastname}
+              id="phonenumber"
+              name="phonenumber"
+              value={formData.phonenumber}
               onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onFocus={() => handleFocus('phonenumber')}
+              onBlur={handleBlur}
+              className={`w-full px-2 py-2 border-b-2 ${errors.phonenumber ? 'border-red-500' : 'border-gray-300'} focus:outline-none`}
             />
+            {errors.phonenumber && <p className="text-red-500 text-sm mt-1">{errors.phonenumber}</p>}
           </div>
         )}
-        {blok.address !== undefined && (
-          <div className="mb-4">
-            <label htmlFor="address" className="block text-gray-700 font-medium mb-1 px-2">
-              Adress
-            </label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-        )}
-        {blok.checklist !== undefined && (
-          <div className="mb-4">
-            <p className="text-gray-700 font-medium mb-2 px-2">Vad behöver ni hjälp med?</p>
-            {blok.Checklist && Array.isArray(blok.Checklist) && blok.Checklist.length > 0 ? (
-              blok.Checklist.map((option, index) => (
-                option ? (
-                  <div key={index} className="flex items-center mb-2 px-2">
-                    <input
-                      type="checkbox"
-                      id={`checkbox-${index}`}
-                      name="checklist"
-                      value={option}
-                      checked={formData.checklist.includes(option)}
-                      onChange={handleChange}
-                      className="mr-2 h-5 w-5 text-blue-600 border-gray-300 border focus:ring-blue-500"
-                    />
-                    <label htmlFor={`checkbox-${index}`} className="text-gray-700">
-                      {option}
-                    </label>
-                  </div>
-                ) : null
-              ))
-            ) : (
-              <p className="text-gray-500">No options available.</p>
-            )}
-          </div>
-        )}
-        {blok.description !== undefined && (
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-700 font-medium mb-1 px-2">
+        {blok.message !== undefined && (
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className={getLabelClasses('message')}
+            >
               Beskrivning
             </label>
             <textarea
-              id="description"
-              name="description"
-              value={formData.description}
+              id="message"
+              name="message"
+              value={formData.message}
               onChange={handleChange}
-              className={`w-full px-3 py-2 border rounded-lg ${errors.description ? 'border-red-500' : 'border-gray-300'} focus:outline-none focus:ring-2 focus:ring-blue-500`}
-              rows="4"
+              onFocus={() => handleFocus('message')}
+              onBlur={handleBlur}
+              className={`w-full px-2 border-b-2 ${errors.message ? 'border-red-500' : 'border-gray-300'} focus:outline-none resize-none`}
+              rows="1"
+              style={{ paddingTop: '1.5rem' }} 
             ></textarea>
-            {errors.description && <p className="text-red-500 text-sm mt-1">{errors.description}</p>}
+            {errors.message && <p className="text-red-500 text-sm mt-1">{errors.message}</p>}
           </div>
         )}
         <button type="submit" className="w-full bg-oakhill-green text-white py-2 rounded-lg hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
@@ -167,7 +208,7 @@ const GenericForm = ({ blok }) => {
         </button>
       </form>
       {notification && (
-        <div className={`mt-4 p-4 rounded-lg ${notification.type === 'success' ? 'bg-green-200 text-green-800' : ' text-red-800'}`}>
+        <div className={`mt-4 p-4 rounded-lg ${notification.type === 'success' ? 'bg-green-200 text-green-800' : 'text-red-800'}`}>
           {notification.message}
         </div>
       )}
