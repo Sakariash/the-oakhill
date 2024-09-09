561ac157e556e3efb93ea426ef0a5cc2120d1329
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-09-10 01:27:06 +0200

    feat: process

--- components/Process.js
+++ components/Process.js
@@ -1,38 +1,41 @@
-const steps = [
-    { number: 1, text: 'Step 1 description' },
-    { number: 2, text: 'Step 2 description' },
-    { number: 3, text: 'Step 3 description' },
-    { number: 4, text: 'Step 4 description' },
-    { number: 5, text: 'Step 5 description' },
+import React from 'react'; // Import React
+import { storyblokEditable } from '@storyblok/react';
+
+const Process = ({ blok }) => {
+
+  const steps = [
+    { value: '1', description: blok.step_1 },
+    { value: '2', description: blok.step_2 },
+    { value: '3', description: blok.step_3 },
+    { value: '4', description: blok.step_4 },
+    { value: '5', description: blok.step_5 }
   ];
-  
-  const Process = () => {
-    return (
-      <div className="flex flex-col items-center bg-oakhill-green mt-14">
-        <h2 className="text-2xl font-bold mb-4">Hur fungerar det?</h2>
-        <div className="flex items-center">
-          {steps.map((step, index) => (
-            <React.Fragment key={step.number}>
-              <div className="flex items-center">
-                <div className="flex items-center justify-center w-8 h-8 border border-gray-400 rounded-full text-center font-bold text-gray-800">
-                  {step.number}
-                </div>
-                {index < steps.length - 1 && (
-                  <div className="w-10 border-t border-gray-400 mx-2" />
-                )}
+
+  return (
+    <div {...storyblokEditable()} className="flex flex-col items-center mt-14 px-4">
+      <h2 className="text-6xl mt-14 font-montserrat">Hur fungerar det?</h2>
+      <div className="flex items-center mt-14">
+        {steps.map((step, index) => (
+          <React.Fragment key={index}>
+            <div className="flex flex-col items-center">
+              <div className="w-20 h-20 border border-oakhill-green rounded-full flex items-center justify-center text-2xl text-gray-800">
+                {step.value}
               </div>
-            </React.Fragment>
-          ))}
-        </div>
-        <div className="flex flex-col items-center mt-4">
-          {steps.map(step => (
-            <div key={step.number} className="text-center mt-2">
-              <p className="font-semibold">{step.text}</p>
+              <p className="mt-2 text-center">{step.description}</p>
             </div>
-          ))}
+            {index < steps.length - 1 && (
+              <div className="w-16 border-t border-oakhill-green mx-4" />
+            )}
+          </React.Fragment>
+        ))}
+      {/* <div className="flex flex-col items-center">
+       <div className="text-4xl text-gray-800">
+            Klart
         </div>
+       </div> */}
       </div>
-    );
-  };
-  
-  export default Process;
+    </div>
+  );
+};
+
+export default Process;
