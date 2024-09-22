4ff7c0ab4830675f4fa32e4b2065c96d88762409
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-09-22 22:01:53 +0200

    feat: grid

--- components/Footer.js
+++ components/Footer.js
@@ -32,12 +32,12 @@ return (
         <span className="absolute -bottom-[6px] left-0 h-[2px] bg-gray-400 transition-all duration-700 ease-in-out w-0 group-hover:w-full"></span>
       </a>
     </div>
-    <div className="col-span-full md:col-span-6 6 flex space-x-8 md:mt-4">
+    <div className="col-span-full md:col-span-5 flex space-x-8 md:mt-4">
       <a href="/privacy" className="text-sm md:text-lg text-gray-500 relative group hover:text-gray-900">Integritetspolicy<span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-gray-400 transition-all duration-700 ease-in-out group-hover:w-full"></span></a>
       <a href="#" className="text-sm md:text-lg text-gray-500 relative group hover:text-gray-900">Användarvillkor<span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-gray-400 transition-all duration-700 ease-in-out group-hover:w-full"></span></a>
       {/* <a href="#" className="text-sm md:text-lg text-gray-500 relative group hover:text-gray-900">Sitemap<span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-gray-400 transition-all duration-700 ease-in-out group-hover:w-full"></span></a> */}
     </div>
-    <div className="col-span-full md:col-span-2 flex md:text-lg text-gray-500 md:mt-4">
+    <div className="col-span-full md:col-span-3 flex md:text-lg text-gray-500 md:mt-4">
      <span className="">© 2024, The Oakhill Design & Solutions</span>
     </div>
   </div>
