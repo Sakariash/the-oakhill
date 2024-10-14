b0959321da9b95e3e20637a6216907523ffce186
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-10-14 21:21:45 +0200

    feat: CTA button

--- components/MenuLink.js
+++ components/MenuLink.js
@@ -1,16 +1,25 @@
 import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const MenuLink = ({ blok }) => {
-  const isContactLink = blok.name === "Kontakta oss"; // Check if it's the "Contact Us" link
+  const isContactLink = blok.name === "Kontakta oss";
 
   return (
-    <Link href={blok.link.cached_url} {...storyblokEditable(blok)}>
-      <div className={isContactLink ? "p-3 bg-black" : "p-3"}>
-        <span className={isContactLink ? "text-base text-white font-medium hover:text-gray-500" : "text-base text-black font-medium hover:text-gray-500"}>
-          {blok.name}
+    <Link
+      href={blok.link.cached_url}
+      {...storyblokEditable(blok)}
+      className={`text-base font-medium relative group p-3 inline-block ${isContactLink ? "bg-black text-white" : "text-black mr-2"}`}
+    >
+      {blok.name}
+      {isContactLink && (
+        <span className="inline-block ml-2 transition-transform duration-700 ease-in-out group-hover:rotate-45 group-hover:translate-y-[2px]">
+          <FiArrowUpRight className="transform transition-transform duration-300 ease-in-out" />
         </span>
-      </div>
+      )}
+      {!isContactLink && (
+        <span className="absolute left-0 bottom-0 h-[1px] bg-gray-400 w-0 transition-all duration-700 ease-in-out group-hover:w-full"></span>
+      )}
     </Link>
   );
 };
