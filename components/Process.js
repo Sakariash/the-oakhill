5b86ff00e3109d414c5f3a2bf8817521f35e64de
sakariash <62565576+Sakariash@users.noreply.github.com>
2024-10-17 01:04:59 +0200

    feat: small changes

--- components/Process.js
+++ components/Process.js
@@ -13,18 +13,18 @@ const Process = ({ blok }) => {
 
   return (
     <div {...storyblokEditable()} className="flex flex-col lg:items-center mt-14 px-4 font-montserrat lg:px-48">
-      <h2 className="text-3xl md:text-5xl lg:text-6xl mt-14">Hur fungerar det?</h2>
+      <h2 className="text-3xl md:text-5xl lg:text-6xl mt-14 text-oakhill-black">Hur fungerar det?</h2>
       <div className="flex flex-col lg:flex-row lg:items-center mt-14">
         {steps.map((step, index) => (
           <React.Fragment key={index}>
             <div className="flex flex-row lg:flex-col lg:items-center lg:w-32 lg:h-48">
-              <div className="min-w-20 h-20 mr-12 border lg:mb-0 lg:mr-0 border-oakhill-green rounded-full flex items-center justify-center text-4xl text-gray-800">
+              <div className="min-w-20 h-20 mr-12 border lg:mb-0 lg:mr-0 border-oakhill-black rounded-full flex items-center justify-center text-4xl text-gray-800">
                 {step.value}
               </div>
               <p className="mt-7 text-left lg:text-center">{step.description}</p>
             </div>
             {index < steps.length - 1 && (
-              <div className="w-16 ml-10 lg:ml-4 border-l lg:border-l-0 lg:h-0 h-16 lg:border-t border-oakhill-green mx-4 lg:mb-24  lg:visible" />
+              <div className="w-16 ml-10 lg:ml-4 border-l lg:border-l-0 lg:h-0 h-16 lg:border-t border-oakhill-black mx-4 lg:mb-24  lg:visible" />
             )}
           </React.Fragment>
         ))}
