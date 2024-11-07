0963afc567fe099de5c6bf8020420205ed45a58e
sakariash <simonsakarias@gmail.com>
2024-11-07 09:50:56 +0100

    feat: fix overflow

--- components/InformationText.js
+++ components/InformationText.js
@@ -13,7 +13,8 @@ const InformationText = ({ blok }) => {
    }
 
   return (
-    <div ref={ref} {...storyblokEditable(blok)} className="mt-28 py-9 relative w-full grid grid-cols-12 grid-rows-10 bg-oakhill-black font-montserrat text-white">
+  
+    <div ref={ref} {...storyblokEditable(blok)} className="mt-28 py-9 relative w-full grid grid-cols-12 grid-rows-10 bg-oakhill-black font-montserrat text-white overflow-hidden">
       {blok.headline && (
   <div className="col-start-2 col-end-12 row-start-1 row-end-5 md:row-start-2 lg:col-start-2 lg:col-end-11  2xl:col-start-2 2xl:col-end-9 2xl:row-end-5 text-left">
         <h2 className={`text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl ${hasAnimated ? 'animate-slideDown' : 'opacity-0'}`}>
