9b3f45bf34a4c52a0eb88d1b20224854195840ad
sakariash <simonsakarias@gmail.com>
2024-11-12 00:28:10 +0100

    trigger

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,5 +1,5 @@
 import Head from "next/head";
- 
+
 import {
   useStoryblokState,
   getStoryblokApi,
@@ -10,50 +10,54 @@ export default function Page({ story, config }) {
   story = useStoryblokState(story);
 
   return (
-    <div >
+    <div>
       <Head>
         <title>{story ? story.name : "My Site"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <StoryblokComponent blok={story.content} />
+      <StoryblokComponent blok={story.content} />
     </div>
   );
 }
- 
+
 export async function getStaticProps({ params }) {
-    let slug = params.slug ? (Array.isArray(params.slug) ? params.slug.join("/") : params.slug) : "home";
-  
-    let sbParams = {
-      version: "draft", // or 'published'
-    };
-  
-    const storyblokApi = getStoryblokApi();
-    let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
-    let { data: config } = await storyblokApi.get('cdn/stories/config', sbParams);
-  
-    return {
-      props: {
-        story: data ? data.story : false,
-        key: data ? data.story.id : false,
-        config: config ? config.story : false,
-      },
-      revalidate: 3600,
-    };
-  }
+  let slug = params.slug
+    ? Array.isArray(params.slug)
+      ? params.slug.join("/")
+      : params.slug
+    : "home";
+
+  let sbParams = {
+    version: "draft", // or 'published'
+  };
+
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
+  let { data: config } = await storyblokApi.get("cdn/stories/config", sbParams);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      key: data ? data.story.id : false,
+      config: config ? config.story : false,
+    },
+    revalidate: 3600,
+  };
+}
 export async function getStaticPaths() {
-    const storyblokApi = getStoryblokApi();
-    let { data } = await storyblokApi.get("cdn/links/", {
-      version: 'draft'
-    });
-  
-    console.log('Data from Storyblok:', data); // Log the data object
-  
-    const paths = Object.values(data.links)
-      .filter(link => !link.is_folder && link.slug !== "home")
-      .map(link => ({ params: { slug: link.slug } }));
-  
-    return {
-      paths: paths,
-      fallback: false,
-    };
-  }
+  const storyblokApi = getStoryblokApi();
+  let { data } = await storyblokApi.get("cdn/links/", {
+    version: "draft",
+  });
+
+  const paths = Object.values(data.links)
+    .filter(
+      (link) => !link.is_folder && link.slug !== "home" && link.slug !== "404"
+    )
+    .map((link) => ({ params: { slug: link.slug } }));
+
+  return {
+    paths: paths,
+    fallback: false, // Or 'blocking' depending on your use case
+  };
+}
